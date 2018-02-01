dfbbdf768ca2d8435626beca402d2d18a805c4f0
Paweł Malon <pawel.malon@phorest.com>
2018-02-01 12:14:41 +0100

    - single digit test implemented

--- src/main/java/com/phorest/tdd/katas/StringCalculator.java
+++ src/main/java/com/phorest/tdd/katas/StringCalculator.java
@@ -2,10 +2,17 @@ package com.phorest.tdd.katas;
 
 public class StringCalculator {
 
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
+    public int add(String numbersString) {
+        if (numbersString.isEmpty()) {
             return 0;
         }
+
+        String[] numbers = numbersString.split(",");
+
+        if (numbers.length == 1) {
+            return Integer.parseInt(numbers[0]);
+        }
+
         throw new UnsupportedOperationException("not implemented yet");
     }
 
