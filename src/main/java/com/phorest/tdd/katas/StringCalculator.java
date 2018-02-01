a39e92e5338683cb5b467ccbcc9c0cec5bc2a6ab
Paweł Malon <pawel.malon@phorest.com>
2018-02-01 13:17:44 +0100

    - summing up five numbers - test passing + refactored

--- src/main/java/com/phorest/tdd/katas/StringCalculator.java
+++ src/main/java/com/phorest/tdd/katas/StringCalculator.java
@@ -1,37 +1,22 @@
 package com.phorest.tdd.katas;
 
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     public int add(String numbersString) {
-        if (numbersString.isEmpty()) {
-            return 0;
-        }
-
-        String[] numbers = getNumbers(numbersString);
-
-        if (numbers.length == 1) {
-            return getFirstNumber(numbers);
-        }
-
-        if (numbers.length == 2) {
-            int firstNumber = getFirstNumber(numbers);
-            int secondNumber = getNumberAtIndex(numbers, 1);
-            return firstNumber + secondNumber;
-        }
-
-        throw new UnsupportedOperationException("not implemented yet");
-    }
-
-    private int getFirstNumber(String[] numbers) {
-        return getNumberAtIndex(numbers, 0);
+        return Stream.of(getNumbers(numbersString))
+                .filter(this::isNumeric)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
     private String[] getNumbers(String numbersString) {
         return numbersString.split(",");
     }
 
-    private int getNumberAtIndex(String[] numbers, int idx) {
-        return numbers.length > idx ?  Integer.parseInt(numbers[idx]) : 0;
+    private boolean isNumeric(String s) {
+        return s.matches("\\d+");
     }
 
 }
