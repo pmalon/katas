b434edd3dd9f2086c21442b910ffe5ebe0e8f9dc
Paweł Malon <pawel.malon@phorest.com>
2018-02-01 12:05:52 +0100

    - first test implemented

--- src/main/java/com/phorest/tdd/katas/StringCalculator.java
+++ src/main/java/com/phorest/tdd/katas/StringCalculator.java
@@ -3,6 +3,9 @@ package com.phorest.tdd.katas;
 public class StringCalculator {
 
     public int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
         throw new UnsupportedOperationException("not implemented yet");
     }
 
